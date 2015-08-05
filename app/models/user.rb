3295f139340db4e79f37fb8c0b41fc6fcc59f59a
Michael Harvey <michaelharvey17@gmail.com>
2015-08-05 11:43:11 -0400

    Validation and password confirmation fixed. Closed #17 and Closed #22

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :drawings
   has_many :descriptions
-  
-  validates :password, confirmation: true
-  validates_uniqueness_of :username
-  validates_presence_of :username
-  validates :username, length: {minimum: 4, maximum: 14}
-  validates :password_digest, confirmation: true, presence: true
+
+  validates :username, presence: true, uniqueness: true, length: {minimum: 4, maximum: 14}
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :password_digest, confirmation: true, presence: true, length: {minimum: 7, maximum: 14}
 end
