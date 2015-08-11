d999e2a394a1a2a04f9649717d6102fb3f3e5ca4
Michael Harvey <michaelharvey17@gmail.com>
2015-08-11 16:48:36 -0400

    added region to fog.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,8 @@ Rails.application.configure do
   :fog_credentials => {
     :provider => "AWS",
     :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    :region => "us-west-2"
   },
   :fog_directory => ENV['S3_BUCKET_NAME']
 }
