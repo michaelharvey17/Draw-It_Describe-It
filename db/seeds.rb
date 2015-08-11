1037375200dba6f67908e109aafcdbb4db53df6b
Ubuntu <ubuntu@ip-172-31-29-251.us-west-2.compute.internal>
2015-08-11 15:58:09 +0000

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(username: mikal, email: admin@thispage.com, password: jumbalaya)
+User.create(username: "mikal", email: "admin@thispage.com", password: "jumbalaya")
 Description.create(user_id: 1, content: "There is no excuse for being Kanye in a post Katrina world.", parent: true, active: true)
